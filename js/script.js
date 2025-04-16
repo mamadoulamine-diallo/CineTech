a18cbd39aaa758722b2730634558c2e914332e2e
sebastien-liveyupeng <sebastien.live-yu-peng@laplateforme.io>
2025-04-16 13:39:14 +0200

    header rework

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+// *******************Script Burger Menu*******************
+document.addEventListener("DOMContentLoaded", function () {
+    const burger = document.querySelector(".hamburger-menu");
+    const sidebar = document.getElementById("sidebar");
+  
+    burger.addEventListener("click", function () {
+      sidebar.classList.toggle("collapsed");
+    });
+  });
